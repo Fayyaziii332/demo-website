9b8c174db58ac023167ad4fdb383291dabfd787b
Fayyaziii332 <54409264+Fayyaziii332@users.noreply.github.com>
2022-03-22 16:31:41 +0500

    made changes for deployment

--- pages/index.js
+++ pages/index.js
@@ -99,19 +99,19 @@ export default function Home() {
                     <h3 className="text-gray-500 mb-3">CEO & Founde</h3>
                     <p className="mb-4">Faiz was an executive at Cisco & played multiple leadership roles.</p>
                     <span className="inline-flex">
-                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank" aria-label="Facebook">
+                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank"   rel="noreferrer" aria-label="Facebook">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank" aria-label="Twitter">
+                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank"   rel="noreferrer" aria-label="Twitter">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank" aria-label="Linkedin">
+                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank"   rel="noreferrer" aria-label="Linkedin">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -131,19 +131,19 @@ export default function Home() {
                     <p className="mb-4">Zayad is Marketing head and Business Development.
                                     .</p>
                     <span className="inline-flex">
-                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank" aria-label="Facebook">
+                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank"   rel="noreferrer" aria-label="Facebook">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank" aria-label="Twitter">
+                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank"   rel="noreferrer" aria-label="Twitter">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank" aria-label="Linkedin">
+                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank"   rel="noreferrer" aria-label="Linkedin">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -164,19 +164,19 @@ export default function Home() {
                     <p className="mb-4">Syed Faizullah is Director Network Solutions Engineering
                                     .</p>
                     <span className="inline-flex">
-                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank" aria-label="Facebook">
+                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank"   rel="noreferrer" aria-label="Facebook">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank" aria-label="Twitter">
+                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank"   rel="noreferrer" aria-label="Twitter">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank" aria-label="Linkedin">
+                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank"   rel="noreferrer" aria-label="Linkedin">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -196,19 +196,19 @@ export default function Home() {
                     <p className="mb-4">Maimoona Iqbal is Company's Engineering Lead
                                     .</p>
                     <span className="inline-flex">
-                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank" aria-label="Facebook">
+                      <a className="text-gray-700" href="https://www.facebbok.com/" target="_blank"   rel="noreferrer" aria-label="Facebook">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank" aria-label="Twitter">
+                      <a className="ml-2 text-gray-700" href="https://www.twitter.com/" target="_blank"   rel="noreferrer" aria-label="Twitter">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank" aria-label="Linkedin">
+                      <a className="ml-2 text-gray-500" href="https://www.linkedin.com/" target="_blank"   rel="noreferrer" aria-label="Linkedin">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                           className="w-6 h-6" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
@@ -352,29 +352,29 @@ export default function Home() {
         <div className="bg-gray-200">
           <nav className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row ">
             <p className="text-gray-700 text-sm text-center sm:text-left mt-1 ">© 2022 —
-                <a href="#" rel="noopener noreferrer" className="text-gray-700 ml-1" target="_blank">Wanclouds Inc</a>
+                <a href="#" className="text-gray-700 ml-1" target="_blank"   rel="noreferrer">Wanclouds Inc</a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a className="text-gray-700" href="https://www.facebook.com/" target="_blank" aria-label="facebook">
+              <a className="text-gray-700" href="https://www.facebook.com/" target="_blank"   rel="noreferrer" aria-label="facebook">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6"
                   viewBox="0 0 24 24">
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-700" href="https://www.twitter.com/" target="_blank" aria-label="Twitter">
+              <a className="ml-3 text-gray-700" href="https://www.twitter.com/" target="_blank"   rel="noreferrer" aria-label="Twitter">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-6 h-6"
                   viewBox="0 0 24 24">
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-700" href="https://www.instagram.com/" target="_blank" aria-label="instagram">
+              <a className="ml-3 text-gray-700" href="https://www.instagram.com/" target="_blank"   rel="noreferrer" aria-label="instagram">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                   className="w-6 h-6" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-700" href="https://www.linkedin.com/" target="_blank" aria-label="linkedin">
+              <a className="ml-3 text-gray-700" href="https://www.linkedin.com/" target="_blank"   rel="noreferrer" aria-label="linkedin">
                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0"
                   className="w-6 h-6" viewBox="0 0 24 24">
                   <path stroke="none"
